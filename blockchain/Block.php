53995e97ea6564229a23403a9b54e960f1b242ed
JosefRosa <76937639+JosefRosa@users.noreply.github.com>
2023-05-07 21:40:27 +0200

    Update and rename Block + Ichain.php to Block.php

--- blockchain/Block.php
+++ blockchain/Block.php
@@ -29,17 +29,3 @@ class Block {
         return $this->hash;
     }
 }
-
-interface IChain {
-    public function addBlock(Block $block): static;
-    public function getBlock(int $id): ?Block;
-    public function getLastBlock(): ?Block;
-    public function isValid(): bool;
-}
-
-$ch = (new Chain)
-    ->addBlock(new Block("Varnsdorf", 0, date('Y-m-d H:i:s'), ""))
-    ->addBlock(new Block("Rumburk", 1, date('Y-m-d H:i:s'), ""));
-var_dump($ch);
-echo "Chain " . ($ch->isValid() ? "is" : "is not") . " valid.\n";
-echo "Poslední město: " . $ch->getLastBlock()->getContent() . "\n";
