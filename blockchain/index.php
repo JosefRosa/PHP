fcbfb44f7cd8599b67f22f23a0375c2ead664e71
JosefRosa <76937639+JosefRosa@users.noreply.github.com>
2023-05-07 21:41:52 +0200

    Update index.php

--- blockchain/index.php
+++ blockchain/index.php
@@ -5,6 +5,5 @@ $ch = (new Chain)
     ->addBlock(new Block("Rumburk", 1, date('Y-m-d H:i:s'), ""));
     
 var_dump($ch);
-
 echo "Chain " . ($ch->isValid() ? "is" : "is not") . " valid.\n";
 echo "Poslední město: " . $ch->getLastBlock()->getContent() . "\n";
